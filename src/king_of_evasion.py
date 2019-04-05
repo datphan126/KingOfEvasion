9dbeebd9100820165fc6331e9f61bc41f6369722
Dustin <dphant126@gmail.com>
2019-04-04 22:09:46 -0700

    added full screen mode

--- src/king_of_evasion.py
+++ src/king_of_evasion.py
@@ -23,8 +23,6 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-ball_size = 50
-
 # Game State
 GAME_STATE_OVER = "GAMEOVER"
 GAME_STATE_MENU = "MENU"
@@ -42,7 +40,9 @@ pygame.init()
  
 # Set the height and width of the screen
 size = [gd.SCREEN_WIDTH, gd.SCREEN_HEIGHT]
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+# Window mode - Testing
+# screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("King Of Evasion")
  
@@ -61,7 +61,8 @@ clock = pygame.time.Clock()
 # Create new ball list
 balls = []
 # Number of balls
-ball_list_size = gd.BALL_LIMIT_EASY
+ball_list_size = 0
+ball_size = 50
 
 # Initiate Mouse position
 mouse_x = 0
@@ -82,7 +83,10 @@ old_score = 0
 countdown = 3
 
 # Select the font to use, size, bold, italics
-font = pygame.font.SysFont('Calibri', 25, True, False)
+normal_font = pygame.font.SysFont('Calibri', 25, True, False)
+title_font_1 = pygame.font.SysFont('Calibri', 50, True, False)
+title_font_2 = pygame.font.SysFont('Calibri', 130, True, False)
+countdown_font = pygame.font.SysFont('Calibri', 105, True, False)
 
 # Create music list
 music_list = []
@@ -95,6 +99,18 @@ def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
+
+# Calculate ball limit based on selected difficulty
+def calculate_ball_limit():
+    global ball_list_size
+    if game_difficulty == GAME_DIFFICULTY_EASY:
+        ball_list_size = round(gd.BALL_LIMIT_EASY * gd.SCREEN_WIDTH)
+    elif game_difficulty == GAME_DIFFICULTY_MED:
+        ball_list_size = round(gd.BALL_LIMIT_MED * gd.SCREEN_WIDTH)
+    elif game_difficulty == GAME_DIFFICULTY_HARD:
+        ball_list_size = round(gd.BALL_LIMIT_HARD * gd.SCREEN_WIDTH)
+    elif game_difficulty == GAME_DIFFICULTY_IMPOSSIBLE:
+        ball_list_size = round(gd.BALL_LIMIT_IMPOSSIBLE * gd.SCREEN_WIDTH)
 
 def create_balls():
     global balls
@@ -127,7 +143,7 @@ def check_collision_with_mouse():
             global game_state
             game_state = GAME_STATE_OVER
             pygame.mixer.music.stop()
-            pygame.mixer.music.load(resource_path("PacManGameOver.mp3"))
+            pygame.mixer.music.load(resource_path("GameOver.mp3"))
             pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
             pygame.mixer.music.play()
             break
@@ -171,9 +187,11 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN and game_state == GAME_STATE_MENU:
+            if event.key == pygame.K_ESCAPE:
+                done = True
+            elif event.key == pygame.K_RETURN and game_state == GAME_STATE_MENU:
                 game_state = GAME_STATE_SELECT_MODE
-            elif event.key == pygame.K_ESCAPE:
+            elif event.key == pygame.K_SPACE:
                 game_state = GAME_STATE_MENU
                 # Play background music
                 pygame.mixer.music.stop()
@@ -183,22 +201,22 @@ while not done:
             elif (event.key == pygame.K_1 or event.key == pygame.K_KP1) and game_state == GAME_STATE_SELECT_MODE:
                 game_difficulty = GAME_DIFFICULTY_EASY
                 game_speed = gd.BALL_MOVE_SPEED_EASY
-                ball_list_size = gd.BALL_LIMIT_EASY
+                calculate_ball_limit()
                 game_state = GAME_STATE_COUNTDOWN
             elif (event.key == pygame.K_2 or event.key == pygame.K_KP2) and game_state == GAME_STATE_SELECT_MODE:
                 game_difficulty = GAME_DIFFICULTY_MED
                 game_speed = gd.BALL_MOVE_SPEED_MED
-                ball_list_size = gd.BALL_LIMIT_MED
+                calculate_ball_limit()
                 game_state = GAME_STATE_COUNTDOWN
             elif (event.key == pygame.K_3 or event.key == pygame.K_KP3) and game_state == GAME_STATE_SELECT_MODE:
                 game_difficulty = GAME_DIFFICULTY_HARD
                 game_speed = gd.BALL_MOVE_SPEED_HARD
-                ball_list_size = gd.BALL_LIMIT_HARD
+                calculate_ball_limit()
                 game_state = GAME_STATE_COUNTDOWN
             elif (event.key == pygame.K_4 or event.key == pygame.K_KP4) and game_state == GAME_STATE_SELECT_MODE:
                 game_difficulty = GAME_DIFFICULTY_IMPOSSIBLE
                 game_speed = gd.BALL_MOVE_SPEED_IMPOSSIBLE
-                ball_list_size = gd.BALL_LIMIT_IMPOSSIBLE
+                calculate_ball_limit()
                 game_state = GAME_STATE_COUNTDOWN
         elif event.type == pygame.constants.USEREVENT:
                 if game_state != GAME_STATE_OVER:
@@ -275,7 +293,7 @@ while not done:
             check_collision_with_reward()
             
             # Draw score
-            textSmallScore = font.render("Score: " + str(score), True, WHITE)
+            textSmallScore = normal_font.render("Score: " + str(score), True, WHITE)
             screen.blit(textSmallScore, [gd.SCREEN_WIDTH - 100, 0])
             
             # Check if balls collide each other
@@ -294,21 +312,45 @@ while not done:
     elif game_state == GAME_STATE_OVER:
         # Show the mouse cursor
         pygame.mouse.set_visible(1)
-        text = font.render("GAME OVER", True, WHITE)
-        screen.blit(text, [200, gd.SCREEN_HEIGHT/2])
-        textScore = font.render(game_difficulty + " - Score: " + str(score), True, WHITE)
-        screen.blit(textScore, [200, gd.SCREEN_HEIGHT/2 + 25])
-        textKey = font.render("Press ESC to return to main menu", True, WHITE)
-        screen.blit(textKey, [200, gd.SCREEN_HEIGHT/2 + 50])
+        
+        text = title_font_1.render("GAME OVER", True, WHITE)
+        # Text height will be increased every time a new line of text is created
+        text_y = gd.SCREEN_HEIGHT/2 - text.get_rect().height/2
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        text = normal_font.render(game_difficulty + " - Score: " + str(score), True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        text = normal_font.render("Press SPACE BAR to return to main menu", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
         
     elif game_state == GAME_STATE_MENU:
         # Show the mouse cursor
         pygame.mouse.set_visible(1)
         countdown = 3
-        text = font.render("BEP Game - King Of Evasion - Touch the Green Orb to gain points", True, WHITE)
-        screen.blit(text, [200, gd.SCREEN_HEIGHT/2])
-        textScore = font.render("Press ENTER to play", True, WHITE)
-        screen.blit(textScore, [200, gd.SCREEN_HEIGHT/2 + 25])
+        
+        text = title_font_2.render("King Of Evasion", True, WHITE)
+        # Text height will be increased every time a new line of text is created
+        text_y = gd.SCREEN_HEIGHT/2 - text.get_rect().height/2
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        
+        # Draw the player sprite on the main menu
+        try:
+            player_sprite = CustomSprite(gd.PLAYER_IMG, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2 - mouse_size*2, text_y - mouse_size])
+            screen.blit(player_sprite.image, player_sprite.rect)
+        except Exception as e:
+            print(e,type(e))    
+                    
+        text_y += text.get_rect().height
+        
+        text = title_font_1.render("Touch the Star to gain points and Evade all asteroids", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        
+        text = title_font_1.render("Press ENTER to play", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
     elif game_state == GAME_STATE_COUNTDOWN:
         # Reset all stats and create balls based on selected difficulty
         reset_stat()
@@ -317,13 +359,12 @@ while not done:
         if countdown < 0:
             game_state = GAME_STATE_PLAY
         else:
-            countdown_font = pygame.font.SysFont('Calibri', 105, True, False)
             if countdown == 0:
                 text = countdown_font.render("GO", True, WHITE)
-                screen.blit(text, [200, gd.SCREEN_HEIGHT/2])
+                screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, gd.SCREEN_HEIGHT/2 - text.get_rect().height/2])
             elif countdown > 0:    
                 text = countdown_font.render(str(countdown), True, WHITE)
-                screen.blit(text, [200, gd.SCREEN_HEIGHT/2])
+                screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, gd.SCREEN_HEIGHT/2 - text.get_rect().height/2])
             # Draw the balls
             for ball in balls:
 #                 pygame.draw.circle(screen, WHITE, [ball.x_pos, ball.y_pos], ball.size)
@@ -333,16 +374,24 @@ while not done:
         pygame.time.delay(1000)
     elif game_state == GAME_STATE_SELECT_MODE:
         pygame.mouse.set_visible(1)
-        text = font.render("Select difficulty:", True, WHITE)
-        screen.blit(text, [200, gd.SCREEN_HEIGHT/2])
-        textScore = font.render("1 - Easy", True, WHITE)
-        screen.blit(textScore, [200, gd.SCREEN_HEIGHT/2 + 25])
-        textScore = font.render("2 - Medium", True, WHITE)
-        screen.blit(textScore, [200, gd.SCREEN_HEIGHT/2 + 50])
-        textScore = font.render("3 - Hard", True, WHITE)
-        screen.blit(textScore, [200, gd.SCREEN_HEIGHT/2 + 75])
-        textScore = font.render("4 - Impossible", True, WHITE)
-        screen.blit(textScore, [200, gd.SCREEN_HEIGHT/2 + 100])
+        
+        # Text height will be increased every time a new line of text is created
+        text_y = gd.SCREEN_HEIGHT/2 - text.get_rect().height/2
+        text = title_font_1.render("Select difficulty:", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        text = title_font_1.render("1 - Easy", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        text = title_font_1.render("2 - Medium", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        text = title_font_1.render("3 - Hard", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
+        text = title_font_1.render("4 - Impossible", True, WHITE)
+        screen.blit(text, [gd.SCREEN_WIDTH/2 - text.get_rect().width/2, text_y])
+        text_y += text.get_rect().height
  
     # --- Wrap-up
     # Limit frames per second
